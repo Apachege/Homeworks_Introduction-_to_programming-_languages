048186cc65c2acde3bc74297f849a41ed16f17a1
Valeriy <apachege@bk.ru>
2022-12-03 21:11:34 +0400

    Add Homework1

---
+++ Homework1/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+// Console.Write("Enter first number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter second number: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a > b)
+// {
+//     Console.WriteLine("Max number = " + a);
+//     Console.WriteLine("Min number = " + b);
+// }
+// else
+// {
+//     Console.WriteLine("Max number = " + b);
+//     Console.WriteLine("Min number = " + a);
+// }
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+
+// Console.Write("Enter first number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter second number: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter third number: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+
+// if (b > max) max = b;
+// if (c > max) max = c;
+
+// Console.Write("Max number = " + max);
+
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+
+// Console.Write("Enter number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+
+// if (a % 2 == 0) 
+// {
+//     Console.Write("Number is even");
+// }
+// else
+// {
+//     Console.Write("Number is odd");
+// }
+
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+
+// Console.Write("Enter number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int count = 2;
+
+// if (a < 2)
+// {
+//     Console.Write("Enter a number greater than 1");
+// }
+// else
+// {
+//     while (count <= a)
+//     {
+//         Console.Write(count + " ");
+//         count+=2;
+//     }
+// }
+
+   
+
